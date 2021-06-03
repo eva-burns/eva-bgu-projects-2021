8bbf6019bc19e2fc992c156a49f5983abf09d5d9
eva-burns <eva.burns@braingu.com>
2021-06-03 15:59:39 -0400

    finished shortest_path

--- toyproblems/src/2021/may/28/shortest_path.py
+++ toyproblems/src/2021/may/28/shortest_path.py
@@ -13,6 +13,33 @@
 # There will only ever be one shortest path for the array. If no path between the first and last node exists, return -1. 
 # The array will at minimum have two nodes. Also, the connection A-B for example, means that A can get to B and B can get to A.
 
-def shortest_path(str):
-  # code goes here
-  return str
+def shortest_path(str_in):
+  nodes = str_in[1:int(str_in[0]) + 1]
+  connect_arr = str_in[int(str_in[0]) + 1: len(str_in)]
+  connections = {n: [] for n in nodes}
+  for i in range(len(connect_arr)):
+    temp = connect_arr[i].split("-")
+    connections[temp[0]].append(temp[1])
+    connections[temp[1]].append(temp[0])
+  shortest = {n: {'weight':1000, 'path':''} for n in nodes}
+  shortest[nodes[0]] = {'weight':0, 'path':nodes[0]}
+  return shortest_path_helper(nodes[0], nodes[int(str_in[0]) - 1], shortest, connections)
+
+def shortest_path_helper(start, end, shortest, connections):
+  while end in shortest.keys() and len(shortest) > 0:
+    [path, shortest] = iter(shortest, connections)
+    print(shortest)
+  if path == "":
+    path = -1
+  return path
+
+def iter(shortest, connections):
+  min_node = min(shortest, key=lambda k: shortest[k]['weight'])
+  for elem in connections[min_node]:
+    if elem in shortest.keys():
+      if shortest[min_node]['weight'] + 1 < shortest[elem]['weight']:
+        shortest[elem]['weight'] = shortest[min_node]['weight'] + 1
+        shortest[elem]['path'] = shortest[min_node]['path'] + "-" + elem
+  path = shortest[min_node]['path']
+  shortest.pop(min_node)
+  return [path, shortest]
