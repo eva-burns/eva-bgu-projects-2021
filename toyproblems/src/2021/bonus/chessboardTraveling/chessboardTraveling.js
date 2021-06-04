6343ba66b80227d1d1ea0b6d9e3e7457f0114ec8
eva-burns <eva.burns@braingu.com>
2021-06-04 15:18:01 -0400

    finished chessboardTraveling

--- toyproblems/src/2021/bonus/chessboardTraveling/chessboardTraveling.js
+++ toyproblems/src/2021/bonus/chessboardTraveling/chessboardTraveling.js
@@ -1,7 +1,24 @@
-// Have the function ChessboardTraveling(str) read str which will be a string consisting of the location of a space on a standard 8x8 chess board with no pieces on the board along with another space on the chess board. The structure of str will be the following: "(x y)(a b)" where (x y) represents the position you are currently on with x and y ranging from 1 to 8 and (a b) represents some other space on the chess board with a and b also ranging from 1 to 8 where a > x and b > y. Your program should determine how many ways there are of traveling from (x y) on the board to (a b) moving only up and to the right. For example: if str is (1 1)(2 2) then your program should output 2 because there are only two possible ways to travel from space (1 1) on a chessboard to space (2 2) while making only moves up and to the right.
+// Have the function ChessboardTraveling(str) read str which will be a string consisting of the location of 
+// a space on a standard 8x8 chess board with no pieces on the board along with another space on the chess 
+// board. The structure of str will be the following: "(x y)(a b)" where (x y) represents the position you 
+// are currently on with x and y ranging from 1 to 8 and (a b) represents some other space on the chess 
+// board with a and b also ranging from 1 to 8 where a > x and b > y. Your program should determine how many 
+// ways there are of traveling from (x y) on the board to (a b) moving only up and to the right. For example: 
+// if str is (1 1)(2 2) then your program should output 2 because there are only two possible ways to travel 
+// from space (1 1) on a chessboard to space (2 2) while making only moves up and to the right.
+
 function chessboardTraveling(str) {
-  // code goes here
-  return str;
+  return chessboardTravelingHelper(parseInt(str[1]), parseInt(str[3]), parseInt(str[6]), parseInt(str[8]));
+}
+
+function chessboardTravelingHelper(currX, currY, targetX, targetY) {
+  if (currX === targetX || currY === targetY) {
+    return 1;
+  } else {
+    let moves = chessboardTravelingHelper(currX + 1, currY, targetX, targetY);
+    moves += chessboardTravelingHelper(currX, currY + 1, targetX, targetY);
+    return moves;
+  }
 }
 
 module.exports = chessboardTraveling;
